283b2d8975ce90113895015ee7475494aca14eff
Yash-Pal-Web <yash9451826087@gmail.com>
2023-03-05 22:32:48 +0530

    Block APP Backend Populate Query

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import blogRouter from "./routes/blog-routes";
+import router from "./routes/user-routes";
+
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(
+    "mongodb://localhost:27017/Blog",{useNewUrlParser :true}
+  )
+  .then(() => app.listen(5000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
